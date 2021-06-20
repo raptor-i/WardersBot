3326766e9d688d4046dfb06118795098a123dcce
raptori <55440860+raptor-i@users.noreply.github.com>
2021-06-20 15:03:09 +0000

    New command and some changes

--- main.py
+++ main.py
@@ -1,33 +1,52 @@
 import os
 import discord
-import commands
-import commands2
+from discord.ext import commands 
+from PIL import Image
+from io import BytesIO
+import comms
+import comms2
 
-
-client = discord.Client()
-
-
+client = commands.Bot(command_prefix = "!")
 
 @client.event
 async def on_ready():
   print("Successfully logged on {0.user}".format(client))
-  discord.Game("!yardim")
+
+@client.command()
+async def wanted(ctx, user : discord.Member = None):
+  if user == None :
+    user = ctx.author
+  Wanted = Image.open("stuffs//wanted.jpg")
+  asset = ctx.author.avatar_url_as(size = 128)
+  data = BytesIO(await asset.read())
+  pfp = Image.open(data)
+  pfp = pfp.resize((382,296))
+  Wanted.paste(pfp, (42,220))
+  Wanted.save("stuffs//wanted.jpg")
+  await ctx.send(file = discord.File("stuffs//wanted.jpg"))
+
+@client.command()
+async def alinti(ctx):
+  await ctx.send(comms.get_quote())
+
+@client.command()
+async def hello(ctx):
+  await ctx.send("Hello")
+
 @client.event
 async def on_message(message):
+  await client.process_commands(message)
   if message.author == client.user:
     return
 
-  if message.content.startswith("!alıntı"):
-    await message.channel.send(commands.get_quote())
-
   if message.content.startswith("!yardim"):
-    await message.channel.send("Komutlar : \n!sa  -*Müslüman bot selam alır*\n!alıntı  -*Rastgele özlü söz yazar*\n!covid  -*Türkiye Covid-19 istatistiklerini gösterir*\n!muz  -*Gerçek hesaplamalar sonucu temsili malafat döndürür*\n!ver  -*Bot versiyon*\n!love  -*2 isim yazarak aralarındaki aşkı ölçer (!love isim1 isim2)*\n!cevir  -*İngilizce metinleri Türkçeye çevirir*\n!tokat  -*Bir arkadaşınızı etiketleyerek tokatlarsınız (!tokat @isim)*\n!valo  -*Valorant performansınızı görüntüler*\n!csgo  -*Cs go hakkında bilinmeyen gerçekleri söyler*\n!oyun  -*Hangi oyun olduğunuzu söyler*\n!vs  -*1V1 atarak kimin daha iyi olduğunu öğrenin (!vs @nick)*\n!evlen  -*Ne zaman evleneceğinizi söyler*\n!maas  -*Maaşınızı yazar*\n!kopek  -*Hangi köpek olduğunuzu gösterir*\n!warders  -*Warders nedir?*\n!ulke  -*Tekrar Doğsaydınız hangi ülkeli olurdunuz?*\n!Daha fazla komut için çalışıyoruz...")
+    await message.channel.send("Komutlar : \n!sa  -*Müslüman bot selam alır*\n!alıntı  -*Rastgele özlü söz yazar*\n!covid  -*Türkiye Covid-19 istatistiklerini gösterir*\n!muz  -*Gerçek hesaplamalar sonucu temsili malafat döndürür*\n!ver  -*Bot versiyon*\n!love  -*2 isim yazarak aralarındaki aşkı ölçer (!love isim1 isim2)*\n!cevir  -*İngilizce metinleri Türkçeye çevirir*\n!tokat  -*Bir arkadaşınızı etiketleyerek tokatlarsınız (!tokat @isim)*\n!valo  -*Valorant performansınızı görüntüler*\n!csgo  -*Cs go hakkında bilinmeyen gerçekleri söyler*\n!oyun  -*Hangi oyun olduğunuzu söyler*\n!vs  -*1V1 atarak kimin daha iyi olduğunu öğrenin (!vs @nick)*\n!evlen  -*Ne zaman evleneceğinizi söyler*\n!maas  -*Maaşınızı yazar*\n!kopek  -*Hangi köpek olduğunuzu gösterir*\n!warders  -*Warders nedir?*\n!ulke  -*Tekrar Doğsaydınız hangi ülkeli olurdunuz?*\nDaha fazla komut için çalışıyoruz...")
 
   if message.content.startswith("!sa"):
     await message.channel.send("as")
 
   if message.content.startswith("!covid"):
-    datas = commands.get_covid()
+    datas = comms.get_covid()
     await message.channel.send("\U0001F4CA Covid-19 Verileri Türkiye \U0001F1F9\U0001F1F7\n\n\U0001F6A9 Toplam Vaka : %d\n\U00002755 Toplam Ölüm : %d\n\U00002705 Toplam İyileşen : %d\n\n\U0001F7E5 Bugünkü Vaka : %d\n\U000026A0 Bugünkü Ölüm : %d\n\n\U0001F4C5 Tarih : %s"%(datas[0],datas[1],datas[2],datas[3],datas[4],datas[5])) 
 
   if message.content.startswith("!ver"):
@@ -36,12 +55,12 @@ async def on_message(message):
   if message.content.startswith("!muz"):
     user = message.author.id
     user = "<@%s>"%(user)
-    await message.channel.send("%s' in Muzu\n%s"%(user,commands.get_Banana()))
+    await message.channel.send("%s' in Muzu\n%s"%(user,comms.get_Banana()))
     
     
   if message.content.startswith("!love"):
     mesaj = message.content.split(" ")
-    result =commands.calc_Love(mesaj[1],mesaj[2])
+    result =comms.calc_Love(mesaj[1],mesaj[2])
     yorum = ""
     yuzde = ""
     if(int(result["percentage"]) < 20):
@@ -87,19 +106,11 @@ async def on_message(message):
     await message.channel.send(gif)
 
   if message.content.startswith("!___cevir"):
-    mesaj =message.content.split(" ")
-    mesaj.remove("!cevir")
-    messaj = ""
-    for msg in mesaj:
-      messaj = messaj + " " + msg
-    result = commands.translate(messaj)["outputs"][0]
-    result = result["output"]
-    user = message.author.id
-    user = "<@%s>"%(user)
-    await message.channel.send("\U0001F4CB %s\n\n%s"%(user,result))
+    await message.channel.send(file = discord.File("stuffs//pp.jpg"))
+    
 
   if message.content.startswith("!tokat"):
-    result = commands.slap(message)
+    result = comms.slap(message)
     await message.channel.send("%s  %s **'i Tokat Manyağı Yapıyor**\n\n"%(result[1],result[0]))
     await message.channel.send("\n"+result[2])
 
@@ -107,14 +118,14 @@ async def on_message(message):
     await message.channel.send("||CS:GO çöp bir oyundur.||")
 
   if message.content.startswith("!valo"):
-    result = commands.valo()
+    result = comms.valo()
     user = message.author.id
     user = "<@%s>"%(user)
     await message.channel.send("\U0001F4C8 %s **'in Maç Performansı**\n\n\n\U0001F464Seçilen Ajan : **%s** \n\n\U0001F1F0 Kill : **%s**\n\n\U0001F1E9 Ölüm : **%s**\n\n\U0001F1E6 Asist : **%s**\n\n\U0001F92C Edilen küfür sayısı : **%s**\n"%(user,result[5],result[1],result[2],result[3],result[4]))
     await message.channel.send(result[0])
 
   if message.content.startswith("!oyun"):
-    result = commands.which_game()
+    result = comms.which_game()
     user = message.author.id
     user = "<@%s>"%(user)
     await message.channel.send("%s **Sen bir oyun olsaydın** \U0001F3AE \n ```fix\n%s\n```\n\n"%(user,result[0]))
@@ -123,24 +134,24 @@ async def on_message(message):
   if message.content.startswith("!evlen"):
     user = message.author.id
     user = "<@%s>"%(user)
-    result = commands.get_married()
+    result = comms.get_married()
     await message.channel.send("\n\n%s ***'in Evleneceği Tarih	:*** ```fix\n%s.%s.%s\n```\U0001F935\U0001F470"%(user,result[2],result[1],result[0]))
 
   if message.content.startswith("!maas"):
     user = message.author.id
     user = "<@%s>"%(user)
-    result = commands.get_salary()
+    result = comms.get_salary()
     await message.channel.send("%s**'in Maaşı :**\n ```css\n%s TL\n```"%(user,result))
 
   if message.content.startswith("!kopek"):
     user = message.author.id
     user = "<@%s>"%(user)
-    result = commands.which_dog()
+    result = comms.which_dog()
     await message.channel.send("\n%s ***Bir köpek olsaydı \U0001F436	:***\n"%(user))
     await message.channel.send("%s"%(result))
     
   if message.content.startswith("!vs"):
-    result = commands2.do_vs(message)
+    result = comms2.do_vs(message)
     await message.channel.send("\n\n%s \t\t\t\t\t **VS** \t\t\t\t\t %s\n\nSeçilen Silah : **%15s**\t\t\t\t\tSeçilen Silah : **%15s**\n\nArmor : **%15s**\t\t\t\t\tArmor : **%15s**\n\nKalan Can : **%10d**\t\t\t\t\t\t\t\tKalan Can : **%10d**\n"%(result[8],result[7],result[0],result[3],result[1],result[4],result[2],result[5]))
 
     await message.channel.send("%s"%(result[9]))
@@ -151,7 +162,7 @@ async def on_message(message):
   if message.content.startswith("!ulke"):
     user = message.author.id
     user = "<@%s>"%(user)
-    result = commands2.Get_Countries()
+    result = comms2.Get_Countries()
     await message.channel.send("\n%s\n```fix\n\U0001F3D9 Yeniden Dünyaya Gelsen Doğacağın Ülke : %s\n```"%(user,result[1]))
     await message.channel.send("%s"%(result[0]))
 
